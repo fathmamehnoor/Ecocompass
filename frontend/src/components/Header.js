a6aae6f9cbcec1dc3fdc34a237176e840ac781d7
Rinshi001 <rinshinmf@gmail.com>
2024-12-28 22:58:56 +0530

    added file upload page

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={styles.header}>
+      <h1 style={styles.logo}>EcoCompass</h1>
+      <nav>
+        <ul style={styles.navList}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+const styles = {
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "10px 20px",
+    borderBottom: "1px solid #ccc",
+    boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.1)",
+    width: "80%", // Adjust width of the header
+    margin: "0 auto", // Center the header horizontally
+    backgroundColor: "#e9f8e9", // Background color for the header only
+    borderRadius: "10px", // Add border radius for rounded corners
+    marginTop:"10px",
+    fontWeight: "bold",
+  },
+  logo: { color: "green", fontWeight: "bold", fontSize: "24px" },
+  navList: {
+    listStyleType: "none",
+    display: "flex",
+    gap: "15px",
+  },
+};
+
+export default Header;

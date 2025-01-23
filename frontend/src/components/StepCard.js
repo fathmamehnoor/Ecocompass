517c67ffd75dabbdb5c541c9601db7c87e68932e
fathmamehnoor <fathmamehnoor@example.com>
2025-01-23 08:44:54 +0530

    Modified styling

--- frontend/src/components/StepCard.js
+++ frontend/src/components/StepCard.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import styles from '../styles/StepCard.module.css';
 
 export const StepCard = ({ title, description }) => {
   return (
-    <div className={styles.stepCard}>
-      <div className={styles.stepTitle}>{title}</div>
-      <div className={styles.stepDescription}>{description}</div>
+    <div className="stepCard bg-green-300 rounded-3xl shadow-xl text-black text-center flex-grow p-32 mx-auto lg:ml-20 lg:w-3/4 sm:w-full gap-4">
+      <div className="stepTitle text-5xl sm:text-3xl font-playfair mb-8">{title}</div>
+      <div className="stepDescription text-3xl sm:text-lg font-playfair mt-8">{description}</div>
     </div>
   );
 };

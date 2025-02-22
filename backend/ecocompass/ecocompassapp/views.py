44e9885091ee78eccc9b239ce608d370da8ac94e
fathmamehnoor <fathmamehnoor@example.com>
2025-02-22 12:43:27 +0530

    Defined endpoints for user registration, login and data retrieval

--- backend/ecocompass/ecocompassapp/views.py
+++ backend/ecocompass/ecocompassapp/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from .models import CustomUser
+from .serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh' : str(refresh),
+        'access' : str(refresh.access_token),
+    }
+
+# Signup API
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response(get_tokens_for_user(user), status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def login_user(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+    user = authenticate(email=email, password=password)
+
+    if user:
+        return Response(get_tokens_for_user(user), status=status.HTTP_200_OK)
+    return Response({"error" : "Invalid email or password"}, status = status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['GET'])
+def get_user_data(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
